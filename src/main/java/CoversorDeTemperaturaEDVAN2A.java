778d1d260513bb12e49e8140c660e252ae75caac
2024105070025 <edivanheriquecombr1@gmail.com>
2025-04-29 11:32:42 -0400

    implementação completa do sistema de coversao

--- src/main/java/CoversorDeTemperaturaEDVAN2A.java
+++ src/main/java/CoversorDeTemperaturaEDVAN2A.java
@@ -34,30 +34,30 @@ public class CoversorDeTemperaturaEDVAN2A {
             double graus = scanner.nextDouble();
 
             switch (escolha) {
-                case 1:
+                case 1 -> {
                     Celsius celsius = new Celsius(graus);
                     System.out.println("************************");
                     System.out.println("Resultado:");
                     System.out.println("Fahrenheit:" + celsius.paraFahrenheit());
                     System.out.println("Kelvin:" + celsius.paraKelvin());
                     System.out.println("************************");
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     Fahrenheit f = new Fahrenheit(graus);
                     System.out.println("************************");
                     System.out.println("Resultado:");
                     System.out.println("Fahrenheit:" + f.paraCelsius());
                     System.out.println("Kelvin:" + f.paraKelvin());
                     System.out.println("************************");
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     Kelvin k = new Kelvin(graus);
                     System.out.println("************************");
                     System.out.println("Resultado:");
                     System.out.println("Fahrenheit:" + k.paraFahrenheit());
                     System.out.println("Celsius:" + k.paraCelsius());
                     System.out.println("************************");
-                    break;
+                }
 
             }
 
--- src/main/java/Fahrenheit.java
+++ src/main/java/Fahrenheit.java
@@ -3,14 +3,13 @@ public class Fahrenheit extends Temperatura {
 
     public Fahrenheit(double valorGraus) {
         super(valorGraus);
+    }
 
+    public double paraCelsius() {
+        return (getGraus() - 32) * 5 / 9;
     }
-    
-    public double paraCelsius(){
-        return (getGraus ()-32)*5/9;
-        
+
+    public double paraKelvin() {
+        return paraCelsius() + 273;
     }
-    
-       public double paraCelsius(){
-        return paraCelsius()+273
 }
